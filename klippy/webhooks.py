bac044c51c17d5fe3c96b5c51aae8b0e2001e99d
Arksine <arksine.code@gmail.com>
2020-02-26 17:55:58 -0500

    webhooks:  Initial implementation
    The webhooks module provides Klipper a printer object with which other host modules may use to register web_server endpoints.
    
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

---
+++ klippy/webhooks.py
@@ -0,0 +1,42 @@
+# Klippy WebHooks registration
+#
+# Copyright (C) 2020 Eric Callahan <arksine.code@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license
+import logging
+
+AVAILABLE_METHODS = ['GET', 'POST']
+
+class WebHooksError(Exception):
+    pass
+
+class WebHooks:
+    def __init__(self):
+        self._endpoints = {}
+        self._hooks = []
+
+    def register_endpoint(self, path, callback, methods=['GET'],
+                          arg_parser="default"):
+        if path in self._endpoints:
+            raise WebHooksError("Path already registered to an endpoint")
+
+        methods = [m.upper() for m in methods]
+        for method in methods:
+            if method not in AVAILABLE_METHODS:
+                raise WebHooksError(
+                    "Requested Method [%s] for endpoint '%s' is not valid"
+                    % (method, path))
+
+        self._endpoints[path] = callback
+        self._hooks.append((path, methods, arg_parser))
+
+    def get_hooks(self):
+        return (list(self._hooks))
+
+    def get_callback(self, path):
+        cb = self._endpoints.get(path, None)
+        if cb is None:
+            msg = "No registered callback for path '%s" % path
+            logging.info(logging.info(msg))
+            raise WebHooksError(msg)
+        return cb

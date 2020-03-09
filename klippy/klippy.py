f1ab3055209db0632d74003012f193c6809c05c0
Arksine <arksine.code@gmail.com>
2020-03-09 19:31:11 -0400

    klippy:  remove post_config event
    The webhooks can be registered using the "klippy:connected" event.
    
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- klippy/klippy.py
+++ klippy/klippy.py
@@ -139,8 +139,6 @@ class Printer:
             self.load_object(config, section_config.get_name(), None)
         for m in [toolhead]:
             m.add_printer_objects(config)
-        # Send a post configure event
-        self.send_event("klippy:post_config")
         # Validate that there are no undefined parameters in the config file
         pconfig.check_unused_options(config)
     def _handle_web_request(self, web_request):
